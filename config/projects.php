63fe6636372d96832c4c8f39ea9ff13721555e77
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-07-17 12:43:44 +0200

    project end - fix

---
+++ config/projects.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+    [
+        "title"            => "Spotify Web",
+        "author"           => "Gary Alvarez",
+        "creation_date"    => "2023-03-22",
+        "last_update"      => "2023-03-27",
+        "collaborators"    => "",
+        "languages"        => [1, 2],
+        "description"      => "A clone of Spotify Web",
+        "link_github"      => "https://github.com/GarySmithAlvarezVenturo/html-css-spotifyweb",
+	    "type_id"              => "1",
+        "image"            => "uploads/spotify.png"
+    ],
+    [
+        "title"            => "Campominato",
+        "author"           => "Gary Alvarez",
+        "creation_date"    => "2023-04-06",
+        "last_update"      => "2023-05-01",
+        "collaborators"    => "",
+        "languages"        => [1, 2, 3],
+        "description"      => "A clone of Minefield",
+        "link_github"      => "https://github.com/GarySmithAlvarezVenturo/js-campominato-dom",
+        "type_id"          => "1",
+        "image"            => "uploads/campominato.png"
+    ],
+    [
+        "title"            => "Social Post",
+        "author"           => "Gary Alvarez",
+        "creation_date"    => "2023-05-03",
+        "last_update"      => "2023-05-03",
+        "collaborators"    => "",
+        "languages"        => [1, 2, 3],
+        "description"      => "A clone of Facebook",
+        "link_github"      => "https://github.com/GarySmithAlvarezVenturo/js-social-posts",
+        "type_id"          => "1",
+        "image"            => "uploads/social_post.png"
+    ],
+    [
+        "title"            => "BoolzApp",
+        "author"           => "Gary Alvarez",
+        "creation_date"    => "2023-05-10",
+        "last_update"      => "2023-06-06",
+        "collaborators"    => "",
+        "languages"        => [1, 2, 3],
+        "description"      => "A clone of WhatsApp",
+        "link_github"      => "https://github.com/GarySmithAlvarezVenturo/vue-boolzapp",
+        "type_id"          => "1",
+        "image"            => "uploads/boolzapp.png"
+    ],
+    [
+        "title"            => "BoolFlix",
+        "author"           => "Gary Alvarez",
+        "creation_date"    => "2023-05-24",
+        "last_update"      => "2023-06-26",
+        "collaborators"    => "",
+        "languages"        => [1, 2, 4],
+        "description"      => "A clone of Nelflix",
+        "link_github"      => "https://github.com/GarySmithAlvarezVenturo/vite-boolflix",
+        "type_id"          => "1",
+        "image"            => "uploads/boolflix.png"
+    ],
+    [
+        "title"            => "Project Html-VueJs",
+        "author"           => "Gary Alvarez",
+        "creation_date"    => "2023-05-29",
+        "last_update"      => "2023-06-05",
+        "collaborators"    => "",
+        "languages"        => [1, 2, 4],
+        "description"      => "A clone of a Website",
+        "link_github"      => "https://github.com/DavideFarci/proj-html-vuejs.git",
+        "type_id"          => "1",
+        "image"            => "uploads/project-html-vueJs.png"
+    ],
+    [
+        "title"            => "DC Comics",
+        "author"           => "Gary Alvarez",
+        "creation_date"    => "2023-06-29",
+        "last_update"      => "2023-07-03",
+        "collaborators"    => "",
+        "languages"        => [5, 6],
+        "description"      => "A clone of a Website of DC Comics",      
+        "link_github"      => "https://github.com/GarySmithAlvarezVenturo/laravel-dc-comics",
+        "type_id"          => "2",
+        "image"            => "uploads/dc_comics.png"
+    ],
+    [
+        "title"            => "Laravel Boolean",
+        "author"           => "Paolo Falco",
+        "creation_date"    => "2023-07-04",
+        "last_update"      => "2023-07-004",
+        "collaborators"    => "Alessandra Casu,Gary Alvarez,Antonio Nicolaci",
+        "languages"        => [5, 6, 4],
+        "description"      => "A invoices organizer",
+        "link_github"      => "https://github.com/GarySmithAlvarezVenturo/laravel-boolean",
+        "type_id"          => "3",
+        "image"            => "uploads/laravel_boolean.png"
+    ],
+];
